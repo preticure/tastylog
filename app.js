a6aa9824f2c14b5fbb17d0204b06eaf0e046fb59
aimiyamaguchi <preticure112771@gmail.com>
2023-12-11 22:34:13 +0900

    セクション16まで

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const app = express();
 app.set("view engine","ejs");
 app.disable("x-powered-by");
 
+// Expose global method to view engine.
+app.use((req, res, next)=>{
+  res.locals.moment = require("moment");
+  res.locals.padding = require("./lib/math/math.js").padding;
+  next();
+});
+
 // Static resource rooting
 app.use(favicon(path.join(__dirname,"/public/favicon.ico")));
 app.use("/public",express.static(path.join(__dirname,"/public")));
@@ -19,24 +26,9 @@ app.use("/public",express.static(path.join(__dirname,"/public")));
 app.use(accesslogger());
 
 // Dynamic resource rooting
-app.use("/",require("./routes/index.js"));
-app.use("/test", async (req, res, next)=>{
-  const { MySQLClient, sql } = require("./lib/database/client.js");
-  var data;
-
-  try{
-    await MySQLClient.connect();
-    data = await MySQLClient.query(
-      await sql("SELECT_SHOP_BASIC_BY_ID"),[1]);
-    console.log(data);
-  } catch (err) {
-    next(err);
-  } finally {
-    await MySQLClient.end();
-  }
-
-  res.end("OKOKOK");
-});
+app.use("/search", require("./routes/search.js"));
+app.use("/shops", require("./routes/shops.js"));
+app.use("/", require("./routes/index.js"));
 
 // Set application log
 app.use(applicationlogger());
