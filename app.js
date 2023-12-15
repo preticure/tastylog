f8763ba8303ce5771d6029944c99736b13086c42
aimiyamaguchi <preticure112771@gmail.com>
2023-12-15 20:07:42 +0900

    確認用。ブラウザバッグ機能実装しとつもりが機能していない。原因分からず。

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const PORT = process.env.PORT;
+const appconfig = require("./config/application.config.js");
+const dbconfig = require("./config/mysql.config.js");
 const path = require("path");
 const logger = require("./lib/log/logger.js");
 const applicationlogger = require("./lib/log/applicationlogger.js");
 const accesslogger = require("./lib/log/accesslogger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
+const cookie = require("cookie-parser");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 const app = express();
 
 // Express setting
@@ -25,7 +29,25 @@ app.use("/public",express.static(path.join(__dirname,"/public")));
 // Set access log
 app.use(accesslogger());
 
+// Set middleware
+app.use(cookie());
+app.use(session({
+  store: new MySQLStore({
+    host: dbconfig.HOST,
+    port: dbconfig.PORT,
+    user: dbconfig.USERNAME,
+    password: dbconfig.PASSWORD,
+    database: dbconfig.DATABASE
+  }),
+  secret: appconfig.security.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  name: "sid"
+}));
+app.use(express.urlencoded({ extended: true }));
+
 // Dynamic resource rooting
+app.use("/account", require("./routes/account.js"));
 app.use("/search", require("./routes/search.js"));
 app.use("/shops", require("./routes/shops.js"));
 app.use("/", require("./routes/index.js"));
@@ -34,6 +56,6 @@ app.use("/", require("./routes/index.js"));
 app.use(applicationlogger());
 
 // Execute web application
-app.listen(PORT, () => {
-  logger.application.info(`Application listening at ${PORT}`);
+app.listen(appconfig.PORT, () => {
+  logger.application.info(`Application listening at :${appconfig.PORT}`);
 });
--- config/application.config.js
+++ config/application.config.js
@@ -1,6 +1,8 @@
-// 1ページに表示するアイテム数の設定//
-
 module.exports = {
+  PORT: process.env.PORT || 3000,
+  security: {
+    SESSION_SECRET: "YOUR-SESSION-SECRET-STRING"
+  },
   search: {
     MAX_ITEMS_PER_PAGE: 5
   }
