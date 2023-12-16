aa31d6352ac641cacf083002f118058b41b5a9db
aimiyamaguchi <preticure112771@gmail.com>
2023-12-17 00:11:29 +0900

    セクション19 セクションハイジャック対策で.envファイルでNODE_ENV=productionでセッションID確認できなくなるはずだが、確認できてしまう。

--- routes/account.js
+++ routes/account.js
@@ -1,4 +1,11 @@
 const router = require("express").Router();
+const { authenticate, authorize, PRIVILEGE } = require("../lib/security/accesscontrol.js");
+
+router.get("/login", (req, res, next) => {
+  res.render("./account/login.ejs", {"message": req.flash("message")});
+});
+
+router.post("/login", authenticate());
 
 router.use("/reviews", require("./account.reviews.js"));
 
