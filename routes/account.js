c698f0e2f7473e2df46e5b5e4754b9377373182a
aimiyamaguchi <preticure112771@gmail.com>
2023-12-23 00:23:46 +0900

    コメントなし

--- routes/account.js
+++ routes/account.js
@@ -1,12 +1,21 @@
 const router = require("express").Router();
 const { authenticate, authorize, PRIVILEGE } = require("../lib/security/accesscontrol.js");
 
+router.get("/", authorize(PRIVILEGE.NORMAL), (req, res, next ) => {
+  res.render("./account/index.ejs");
+});
+
 router.get("/login", (req, res, next) => {
   res.render("./account/login.ejs", {"message": req.flash("message")});
 });
 
 router.post("/login", authenticate());
 
-router.use("/reviews", require("./account.reviews.js"));
+router.post("/logout", (req, res, next) => {
+  req.logout();
+  res.redirect("/account/login");
+});
+
+router.use("/reviews", authorize(PRIVILEGE.NORMAL), require("./account.reviews.js"));
 
 module.exports = router;
